cb21b1ef2c6b4322d8c6e30fe0f8369003737788
chemyoo <chemyoo@foxmail.com>
2019-04-13 14:13:10 +0800

    format
    Signed-off-by: chemyoo <chemyoo@foxmail.com>

--- src/com/chemyoo/utils/IdCardUtils.java
+++ src/com/chemyoo/utils/IdCardUtils.java
@@ -1,13 +1,12 @@
 package com.chemyoo.utils;
 
 public class IdCardUtils {
+	
+	private IdCardUtils() {}
+	
 	private static int[] w = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
 	private static char[] verifyCode = "10X98765432".toCharArray();
-
-	public static void main(String[] args) {
-		System.out.println(isValid("362422199207222511"));
-		
-	}
+	private static char zero = '0';
 
 	public static boolean isValid(String id) {
 		if (id.length() < 18) {
@@ -16,7 +15,7 @@ public class IdCardUtils {
 		char[] c = id.toCharArray();
 		int sum = 0;
 		for (int i = 0; i < w.length; i++) {
-			sum += (c[i] - '0') * w[i];
+			sum += (c[i] - zero) * w[i];
 		}
 		char ch = verifyCode[sum % 11];
 		return c[17] == ch;
